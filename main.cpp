36e090c3d8ac6f6ec5cdd4269cf72972e8c4f14c
mshobita <mariam.shobita.ext@orange.com>
2023-12-26 17:21:04 +0200

    If and foreach video#7

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "Wifi.hpp"
 int main() {
+    #ifdef DEBUGINFO
+    std::cout<<DEBUGINFO<<std::endl;
+    #endif
     WIFI_Init();
     // std::cout << "Hello" << std::endl;
     return 0;
